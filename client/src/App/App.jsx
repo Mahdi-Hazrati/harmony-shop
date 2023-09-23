9d99397a2e9cc560554997360630d28a9ea6f5cc
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-09-23 22:25:21 +0330

    install and config react-router-dom

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -1,9 +1,29 @@
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
 import './App.css';
+
+const routersArray = createBrowserRouter([
+  {
+    path:"/",
+    element: <div>Home</div>
+  },
+  {
+    path:"/products/:id",
+    element: <div>Category</div>
+  },
+  {
+    path:"/product/:id",
+    element: <div>Product</div>
+  },
+])
 
 function App() {
   return (
     <div className="app">
-      <h1>React App - init</h1>
+      <RouterProvider router={routersArray} />
     </div>
   );
 }
