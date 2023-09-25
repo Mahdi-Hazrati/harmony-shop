33af7798935dbba28c5d9d520958742c27b18cb6
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-09-25 23:33:12 +0330

    add header main section jsx structure

--- client/src/components/Header/Main.jsx
+++ client/src/components/Header/Main.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import logo_image from "../../assets/images/harmony-shop-logo.svg"
+import { AiOutlineSearch, AiOutlineShopping, AiOutlineUser } from "react-icons/ai"
+import { IoIosArrowDown } from "react-icons/io"
 import "./Main.scss"
 const Main = () => {
   return (
-    <div className="header-main">اصلی</div>
+    <div className="header-main">
+      <div className="brand-container">
+        <img src={logo_image} alt="harmony shop logo" className="brand-logo" />
+        <span className="brand-title">هارمونی شاپ</span>
+      </div>
+      <div className="search-box-container">
+        <input type="text" className="search-box-input" />
+        <AiOutlineSearch className="search-box-icon" />
+      </div>
+      <div className="user-account-container">
+        <div className="right">
+          <AiOutlineUser className="icon" />
+        </div>
+        <div className="center">
+          <span className="hi-message">سلام!</span>
+          <span className="username">مهدی حضرتی</span>
+        </div>
+        <div className="left">
+          <IoIosArrowDown className="icon" />
+
+        </div>
+
+      </div>
+      <div className="shopping-cart-container">
+        <AiOutlineShopping className="icon" />
+        <div className="counter">5</div>
+      </div>
+
+    </div>
   )
 }
 
