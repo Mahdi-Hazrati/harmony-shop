66456cb405e673a3d9aae2e23671136d8bb50c75
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-09-26 11:20:31 +0330

    Inital Home Product Products page style

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import "./Products.scss"
 const Products = () => {
   return (
-    <div>Products</div>
+    <div className="products-page">صفحه ی دسته بندی محصولات</div>
   )
 }
 
